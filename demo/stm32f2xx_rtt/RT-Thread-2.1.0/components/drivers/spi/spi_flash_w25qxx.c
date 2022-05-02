331c31cc9b4fe3b79a2c922e2f98286f7b016043
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2022-05-02 09:35:33 +0000

    chore: rewrite text after encoding conversion
    Patch for b6e81be0c7966cdb9494794367bf305e4bda6cb3 .

--- demo/stm32f2xx_rtt/RT-Thread-2.1.0/components/drivers/spi/spi_flash_w25qxx.c
+++ demo/stm32f2xx_rtt/RT-Thread-2.1.0/components/drivers/spi/spi_flash_w25qxx.c
@@ -41,7 +41,7 @@
 
 #define PAGE_SIZE           4096
 
-/* JEDEC Manufacturer锟斤拷s ID */
+/* JEDEC Manufacturer’s ID */
 #define MF_ID           (0xEF)
 
 /* JEDEC Device ID: Memory type and Capacity */
